f1d0df31be66f211816846b241c351e0fe584f45
tonybnya <nya.tony2010@gmail.com>
2024-06-19 11:50:21 +0100

    feat: add Stack implementation using list

---
+++ stack.py
@@ -0,0 +1,42 @@
+"""
+Implementation of a Stack using a list.
+"""
+
+from typing import Iterable
+
+
+class Stack:
+    """Define a stack."""
+
+    def __init__(self) -> None:
+        """Initialize an empty list."""
+        self.stack = []
+
+    def is_empty(self) -> bool:
+        """Check is the stack is empty or not."""
+        return len(self.stack) == 0
+
+    def push(self, n: int) -> None:
+        """Add an element to the stack."""
+        self.stack.append(n)
+
+    def peek(self) -> int:
+        """Get the last element added."""
+        return self.stack[-1]
+
+    def pop(self) -> int:
+        """Remove and return the last element added."""
+        return self.stack.pop()
+
+    def length(self) -> int:
+        """Get the length of the stack."""
+        return len(self.stack)
+
+    def print_stack(self) -> None:
+        """Print the stack."""
+        n: int = self.length()
+        i: int = 0
+        while i < n:
+            print(f"|{self.stack[i]}|")
+            i += 1
+        print()
